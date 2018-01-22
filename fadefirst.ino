8ee8a13d12b143dd8a3178d7c6653853d403b7e9
Nathan Shorter <7834850+greatn8@users.noreply.github.com>
2018-01-22 19:44:55 +1100

    Add files via upload

---
+++ fadefirst.ino
@@ -0,0 +1,76 @@
+// color swirl! connect an RGB LED to the PWM pins as indicated
+// in the #defines
+// public domain, enjoy!
+
+#include "IRLibAll.h"
+//Create a receiver object to listen on pin 2
+IRrecvPCI myReceiver(2);
+ 
+//Create a decoder object 
+IRdecode myDecoder;   
+ 
+ 
+#define REDPIN 6
+#define GREENPIN 9
+#define BLUEPIN 5
+ 
+#define FADESPEED 5     // make this higher to slow down
+#define MY_PROTOCOL NEC
+#define RIGHT_ARROW   0xfd50af //Move several clockwise
+#define LEFT_ARROW    0xfd10ef //Move servo counterclockwise
+#define SELECT_BUTTON 0xfd906f //Center the servo
+#define UP_ARROW      0xfda05f //Increased number of degrees servo moves
+#define DOWN_ARROW    0xfdb04f //Decrease number of degrees servo moves
+
+void setup() {
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+  myReceiver.enableIRIn(); // Start the receiver
+  Serial.begin(9600);
+  delay(2000); while (!Serial); //delay for Leonardo
+  myReceiver.enableIRIn(); // Start the receiver
+  Serial.println(F("Ready to receive IR signals"));
+}
+ 
+ 
+void loop() {
+  //Continue looping until you get a complete signal received
+  if (myReceiver.getResults()) {
+    myDecoder.decode();           //Decode it
+    myDecoder.dumpResults(true);  //Now print results. Use false for less detail
+    myReceiver.enableIRIn();      //Restart receiver
+  }
+  int r, g, b;
+ 
+  // fade from blue to violet
+  for (r = 0; r < 256; r++) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from violet to red
+  for (b = 255; b > 0; b--) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from red to yellow
+  for (g = 0; g < 256; g++) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+  // fade from yellow to green
+  for (r = 255; r > 0; r--) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from green to teal
+  for (b = 0; b < 256; b++) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from teal to blue
+  for (g = 255; g > 0; g--) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+}
